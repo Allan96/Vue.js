c0d5a2cccb76c97e29240bc2ad1ac51d79846250
Allan96 <allan.souza.silva@usp.br>
2019-12-21 21:23:32 -0300

    Aula 1 - Primeiros Passos

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  <div id="slogan" class="text-center">
+  <h1>NameGator <i class="fa fa-check" aria-hidden="true"></i></h1>
+  <br />
+  <h6 class="text-secondary">Gerador de nomes utilizando Vue.js GraphQl e Node</h6>
   </div>
+  <div id="main">
+    <div class="container">
+      <div class="row">
+        <div class="col-md">
+          <h5>Prefixos <span class="badge badge-info">{{ prefixes.length }}</span></h5>
+          <div class="card">
+             <div class="card-body">
+              <ul class="list-group">
+                <li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
+                  {{ prefix }}
+                </li>
+              </ul>
+              <br/>
+                <input type="text" class="form-control" placeholder="Digite o Prefixo" />
+            </div>
+        </div>
+      </div>
+        <div class="col-md">
+          <h5>Sufixos  <span class="badge badge-info">{{ sufixes.length }}</span></h5>
+          <div class="card">
+            <div class="card-body">
+              <ul class="list-group">
+                <li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">
+                  {{ sufix }}
+                </li>
+              </ul>
+              <br/>
+              <input type="text" class="form-control" placeholder="Digite o Sufixo" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <br/>
+    <h5>Dominios  <span class="badge badge-info">{{ domains.length }}</span></h5>
+    <div class="card">
+      <div class="card-body">
+        <ul class="list-group">
+          <li class="list-group-item" v-for="domain in domains" v-bind:key="domain">
+              {{ domain }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    import "bootstrap/dist/css/bootstrap.css";
+    import "font-awesome/css/font-awesome.css";
+    export default {
+        name: 'app',
+        data: function() {
+            return {
+                prefixes: ['Air', 'Jet', 'Flight'],
+                sufixes: ['Hub', 'Station', 'Mart'],
+                domains: ['AirHub', 'AirStation', 'AirMart', 'JetHub', 'JetSation', 'JetMart', 'FligthHub', 'FligthStation', 'FligthMart']
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #slogan {
+        margin: 30px 0px
+    }
+    
+    #main {
+        background-color: #f1f1f1;
+        padding: 30px 0px;
+    }
 </style>
