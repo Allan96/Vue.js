7e5d819ebdfde58ccbee9f3ccdcd09cb2153fa50
Allan96 <allan.souza.silva@usp.br>
2019-12-29 21:08:18 -0300

    Aula #8

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,7 +6,7 @@
   <h6 class="text-secondary">Gerador de nomes utilizando Vue.js GraphQl e Node</h6>
   </div>
   <div id="main">
-    <DomainsList></DomainsList>
+  <router-view></router-view>
 </div>
 </div>
 </template>
@@ -14,12 +14,8 @@
 <script>
     import "bootstrap/dist/css/bootstrap.css";
     import "font-awesome/css/font-awesome.css";
-    import DomainsList from "./components/DomainsList"
     export default {
         name: "app",
-        components: {
-            DomainsList
-        }
     };
 </script>
 
--- client/src/components/DomainsList.vue
+++ client/src/components/DomainsList.vue
@@ -1,109 +1,164 @@
 <template>
-  <div>
-  <div id="main">
-    <div class="container">
-      <div class="row">
-        <div class="col-md">
-          <AppItemList title="Prefixos" v-bind:items="prefixes" v-on:addItem="addPrefix" v-on:removeItem="removePrefix"></AppItemList>
-      </div>
-        <div class="col-md">
-          <AppItemList title="Sufixos" v-bind:items="sufixes"  v-on:addItem="addSufix" v-on:removeItem="removeSufix"></AppItemList>
-      </div>
-    </div>
-    <br/>
-    <h5>Dominios  <span class="badge badge-info">{{ domains.length }}</span></h5>
-    <div class="card">
-      <div class="card-body">
-        <ul class="list-group">
-          <li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
-            <div class="row">
-              <div class="col-md">
-                {{ domain.name }}
-              </div>
-              <div class="col-md text-right">
-                <a type="button" class="btn btn-info" v-bind:href="domain.checkout" target="_blank"> <i class="fa fa-shopping-cart" aria-hidden="true"></i> </a>
-              </div>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md">
+				<AppItemList title="Prefixos" type="prefix" v-bind:items="items.prefix" v-on:addItem="addItem" v-on:deleteItem="deleteItem"></AppItemList>
+			</div>
+			<div class="col-md">
+				<AppItemList title="Sufixos" type="suffix" v-bind:items="items.suffix" v-on:addItem="addItem" v-on:deleteItem="deleteItem"></AppItemList>
+			</div>
+		</div>
+		<br/>
+		<h5>Domínios <span class="badge badge-info">{{ domains.length }}</span></h5>
+		<div class="card">
+			<div class="card-body">
+				<ul class="list-group">
+					<li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
+						<div class="row">
+							<div class="col-md-6">
+								{{ domain.name }}
+							</div>
+							<div class="col-md-3">
+								<span class="badge badge-info">{{ (domain.available) ? "Disponível" : "Não Disponível" }}</span>
+							</div>
+							<div class="col-md-3 text-right">
+								<a class="btn btn-info" v-bind:href="domain.checkout" target="_blank">
+									<span class="fa fa-shopping-cart"></span>
+								</a>
+								&nbsp;
+								<button class="btn btn-info" @click="openDomain(domain)">
+									<span class="fa fa-search"></span>
+								</button>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-    import axios from "axios/dist/axios";
-    import AppItemList from "./AppItemList";
-    export default {
-        name: "app",
-        components: {
-            AppItemList
-        },
-        data: function() {
-            return {
-                prefixes: ["Air", "Jet", "Flight"],
-                sufixes: ["Hub", "Station", "Mart"],
-            };
-        },
-        methods: {
-            addPrefix(prefix) {
-                this.prefixes.push(prefix);
-            },
-            addSufix(sufix) {
-                this.sufixes.push(sufix);
-            },
-            removeSufix(sufix) {
-                this.sufixes.shift(sufix);
-            },
-            removePrefix(sufix) {
-                this.prefixes.shift(sufix);
-            }
-        },
-        computed: {
-            domains() {
-                // console.log("generating domains...");
-                const domains = [];
-                for (const prefix of this.prefixes) {
-                    for (const sufix of this.sufixes) {
-                        const name = prefix + sufix;
-                        const url = name.toLowerCase();
-                        const checkout = `https://checkout.hostgator.com.br/?a=add&sld=${url}&tld=.com`;
-                        domains.push({
-                            name,
-                            checkout
-                        });
-                    }
-                }
-                return domains;
-            }
-        },
-        created(){
-           axios({
-             url: "http://localhost:4000",
-             method: "post",
-             data:{
-               query: `
-               {
-                prefixes : items (type: "prefix"){
-                   id
-                   type
-                   description
-                  }
-                  sufixes : items (type: "sufix"){
-                   description
-                  }
-               }
-               `
-             }
-           }).then(response =>{
-            const query = response.data;
-            this.prefixes = query.data.prefixes.map(prefix => prefix.description);
-            this.sufixes = query.data.sufixes.map(sufix => sufix.description);
-           });
-        }
-    };
+import axios from "axios/dist/axios";
+import AppItemList from "./AppItemList";
+
+export default {
+	name: "app",
+	components: {
+		AppItemList
+	},
+	data: function () {
+		return {
+			items: {
+				prefix: [],
+				suffix: []
+			},
+			domains: []
+		};
+	},
+	methods: {
+		addItem(item) {
+			axios({
+				url: "http://localhost:4000",
+				method: "post",
+				data: {
+					query: `
+						mutation ($item: ItemInput) {
+							newItem: saveItem(item: $item) {
+								id
+								type
+								description
+							}
+						}
+					`,
+					variables: {
+						item
+					}
+				}
+			}).then(response => {
+				const query = response.data;
+				const newItem = query.data.newItem;
+				this.items[item.type].push(newItem);
+				this.generateDomains();
+			});
+		},
+		deleteItem(item) {
+			axios({
+				url: "http://localhost:4000",
+				method: "post",
+				data: {
+					query: `
+						mutation ($id: Int) {
+							deleted: deleteItem(id: $id)
+						}
+					`,
+					variables: {
+						id: item.id
+					}
+				}
+			}).then(() => {
+				this.items[item.type].splice(this.items[item.type].indexOf(item), 1);
+				this.generateDomains();
+			});
+		},
+		getItems(type) {
+			return axios({
+				url: "http://localhost:4000",
+				method: "post",
+				data: {
+					query: `
+						query ($type: String) {
+							items: items (type: $type) {
+								id
+								type
+								description
+							}
+						}
+					`,
+					variables: {
+						type
+					}
+				}
+			}).then(response => {
+				const query = response.data;
+				this.items[type] = query.data.items;
+			});
+		},
+		generateDomains() {
+			axios({
+				url: "http://localhost:4000",
+				method: "post",
+				data: {
+					query: `
+						mutation {
+							domains: generateDomains {
+								name
+								checkout
+								available
+							}
+						}
+					`
+				}
+			}).then((response) => {
+				const query = response.data;
+				this.domains = query.data.domains;
+			});
+		},
+		openDomain(domain) {
+			this.$router.push({
+				path: `/domains/${domain.name}`
+			});
+		}
+	},
+	created() {
+		Promise.all([
+			this.getItems("prefix"),
+			this.getItems("suffix")
+		]).then(() => {
+			this.generateDomains();
+		});
+	}
+};
 </script>
 
 <style>
